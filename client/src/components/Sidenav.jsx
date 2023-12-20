778d174750bd28b007d12c248e531ba809dc4a33
kaziashraf947 <145238888+kaziashraf947@users.noreply.github.com>
2023-12-20 00:59:52 -0500

    commit

--- client/src/components/Sidenav.jsx
+++ client/src/components/Sidenav.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {AiOutLineMenu} from 'react-icons/ai'
 import React, {useState} from 'react'
 const Sidenav = () => {
+    const [nav, setNav]= useState(false);
   return (
     <div>
         <AiOutLineMenu/>
