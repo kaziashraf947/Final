9d8080cc506d7045e65cd9c617e349684847f149
kaziashraf947 <145238888+kaziashraf947@users.noreply.github.com>
2023-12-19 19:31:39 -0500

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,4 +6,12 @@ module.exports = {
   },
   plugins: [],
 }
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
 
